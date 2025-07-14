7c6ad3e20c1dfbf0d59c777710043275b0270338
ParkGyumin <gyumin1129@kookmin.ac.kr>
2025-07-14 16:19:34 +0900

    DB data 업로드

--- src/main/pipeline.py
+++ src/main/pipeline.py
@@ -32,7 +32,7 @@ class TEPPipeline:
     4. Model4: 정상 여부 재분류 (Model1 재사용)
     """
     
-    def __init__(self, normal_db: np.ndarray = None, data_path: str = 'data/train_fault_0_X.npy'):
+    def __init__(self, normal_db: np.ndarray = None, data_path: str = 'data/normal_db.npy'):
         """
         파이프라인 초기화
         
